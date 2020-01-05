054f63efef10785f5066580fefbef7129d54ada6
mesabuca <msbuyukcaglayan@gmail.com>
2020-01-05 20:22:16 +0300

    Book class methods added

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,25 @@ class Book
   include Mongoid::Document
   field :name, type: String
   field :summary, type: String
+
+  def save(options = {})
+    if new_record?
+      !insert(options).new_record?
+    else
+      update_document(options)
+    end
+  end
+
+  def destroy(options = nil)
+    raise Errors::ReadonlyDocument.new(self.class) if readonly?
+    self.flagged_for_destroy = true
+    result = run_callbacks(:destroy) { delete(options || {}) }
+    self.flagged_for_destroy = false
+    result
+  end
+
+  def update(attributes = {})
+    assign_attributes(attributes)
+    save
+  end
 end
